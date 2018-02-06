9254e8f38e77816c51dd64a3edbb223004059c8b
NopyJF <Nithichai@users.noreply.github.com>
2018-02-06 17:04:47 +0700

    Update Code

--- 2/1.c
+++ 2/1.c
@@ -3,9 +3,9 @@
 int main(void) {
 	P1->SEL1 &= 0x00;
 	P1->SEL0 &= 0x00;
-	P1->DIR &= ~2;
-	P1->REN |= 2;
-	P1->OUT |= 2;
+	P1->DIR &= ~0x10;
+	P1->REN |= 0x10;
+	P1->OUT |= 0x10;
 
 	P2->SEL1 &= ~1;
 	P2->SEL0 &= ~1;
@@ -14,7 +14,7 @@ int main(void) {
 	P2->DIR |= 0x02;
 
 	while(1) {
-		if (P1->IN & 2)
+		if (P1->IN & 0x10)
 			// GREEN 0x02
 			// BLUE 0x04
 			P2->OUT &= ~0x02;
